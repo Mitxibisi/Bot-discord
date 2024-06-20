18cf95425740612e0ff2e10ce78540aedbd4f9dc
Alain <150141630+Mitxibisi@users.noreply.github.com>
2024-06-20 07:07:44 +0200

    Agregar archivos de proyecto.

---
+++ index.js
@@ -0,0 +1,23 @@
+//Archivo Index.js
+const { Client, Events } = require("discord.js");
+
+//Crear nuevo cliente de discord
+const client = new Client({
+    intents : 3276799
+});
+
+//Primer Evento
+client.on(Events.ClientReady, async () => {
+    console.log(`Conectado como ${client.user.username}!`);
+});
+client.on(Events.MessageCreate, async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(`-`)) return;
+
+    const args = message.content.slice(1)
+
+    if(args === "hola") message.reply("Hola")
+})
+
+//Conectar cliente
+client.login("MTI1MjcwMTM5NTk2NzAyMTA3Ng.GD4Nb6.rAx_FPlZPv6hipVLW-IbxSqoV11Srg_O-opMWc");

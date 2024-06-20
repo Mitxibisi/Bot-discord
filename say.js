e14f5cb3d6fc3550871634bbe03a03d3f74f94c9
Alain <150141630+Mitxibisi@users.noreply.github.com>
2024-06-20 18:14:01 +0200

    Actualizar say.js

--- say.js
+++ say.js
@@ -3,6 +3,6 @@ description: 'Repite los argumentos dados',
 run: async ( mensaje) => {
 const args = message.content.split(``).slice(1).join('');
 
-if(args.length < 1) return message.reply(`Error`)
+if(args.length < 1) return message.reply(`ErrorLecMensaje`);
 message.reply(args);
 }

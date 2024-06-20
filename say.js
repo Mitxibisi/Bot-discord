a3b13d426e7f9a6a1c1fc104e5a8ca5b0f01b020
Alain <150141630+Mitxibisi@users.noreply.github.com>
2024-06-20 18:12:06 +0200

    Actualizar say.js

--- say.js
+++ say.js
@@ -1,1 +1,7 @@
-q
+module.exports = { 
+description: 'Repite los argumentos dados',
+run: async ( mensaje) => {
+const args = message.content.split(``).slice(1).join('');
+
+if(args.length < 1) return message.reply(``)
+}

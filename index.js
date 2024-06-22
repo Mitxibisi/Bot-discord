ce44e096a5d8f69ec57b3c112dd68e2b4afb9e40
Alain <150141630+Mitxibisi@users.noreply.github.com>
2024-06-22 11:46:29 +0200

    V1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //Archivo Index.js
-const { Client, Events } = require("discord.js");
+const { Client, Events } = require('discord.js');
 
 //Crear nuevo cliente de discord
 const client = new Client({
-    intents : 3276799
+    intents: 3276799
 });
 
 //Primer Evento
@@ -12,11 +12,31 @@ client.on(Events.ClientReady, async () => {
 });
 client.on(Events.MessageCreate, async (message) => {
     if (message.author.bot) return;
-    if (!message.content.startsWith(`-`)) return;
+    if (!message.content.startsWith('-')) return;
 
-    const args = message.content.slice(1)
+    const args = message.content.slice(1).split(' ')[0]
 
-    if(args === "hola") message.reply("Hola")
+    try {
+        const command = require(`./commands/${args}`);
+        command.run(message);
+    } catch (error) {
+        console.log(`Error general al usar el comando ${args}`, error.message)
+        return message.reply("Sin comando seleccionado")
+    }
+})
+
+client.on(Events.GuildMemberAdd, async (member) => {
+    const welcomeChannelId = '732006205454811206';
+    const channel = await client.channels.fetch(welcomeChannelId);
+
+    channel.send(`**<@${member.user.id}> bienvenido a la comunidad**`);
+})
+
+client.on(Events.GuildMemberRemove, async (member) => {
+    const welcomeChannelId = '732006205454811206';
+    const channel = await client.channels.fetch(welcomeChannelId);
+
+    channel.send(`**<@${member.user.id}> Y SE MARCHOOOO**`);
 })
 
 //Conectar cliente

d557b8d1d133fd96974cb5ca79ad76f7da0a6081
Alain <alainfervi@gmail.com>
2025-02-03 12:49:37 +0000

    await

--- Commands/guildinfo.js
+++ Commands/guildinfo.js
@@ -3,7 +3,7 @@ import {getGuild} from '../GuildsConfig/configs.js'
 
 export async function run(message) {
     if (message.author.id === config.userId){
-const Guild = getGuild(message.guild.id);
+const Guild = await getGuild(message.guild.id);
         console.log(Guild);
     }
 }

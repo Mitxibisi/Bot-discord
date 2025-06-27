965b3f5e3696cca8dc506dbe1335b141f5d7a965
Mitxibisi <150141630+Mitxibisi@users.noreply.github.com>
2025-06-27 11:46:54 +0200

    Delete Commands directory

--- Commands/games/ajedrez.js
+++
@@ -1,3 +0,0 @@
-export async function run(Pl1,Pl2, message) {
-    message.reply('Aun le queda');
-}
--- Commands/guildinfo.js
+++
@@ -1,9 +0,0 @@
-import {client, config} from '../index.js'
-import {getGuild} from '../GuildsConfig/configs.js'
-
-export async function run(message) {
-    if (message.author.id === config.userId){
-const Guild = await getGuild(message.guild.id);
-        console.log(Guild);
-    }
-}
